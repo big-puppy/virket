c847d4098243c17ed3683da534584fe76d6eaf4b
Gabriel hernandez <jgabrielohm@gmail.com>
2021-05-19 17:22:30 -0500

    descubre y ofertas

--- src/app/inicio/inicio.page.ts
+++ src/app/inicio/inicio.page.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../services/api.service';
-
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
@@ -9,28 +8,39 @@ import { ApiService } from '../services/api.service';
 
 export class InicioPage implements OnInit {
 
-  datosPerfil;
-  productos;
-  
+  datosPerfil = { picture: {} };
+  productos = [] ;
+  ofertas = [] ;
+
   constructor(public apis: ApiService) { }
 
-  ngOnInit() {
-    this.obtenerDatosPerfil();
-    this.obtenerProductos();
-  }
+ngOnInit() {
+  this.obtenerDatosPerfil();
+  this.obtenerProductos();
+}
 
-  obtenerDatosPerfil() {
-    this.apis.userProfile().subscribe(response => {
-      this.datosPerfil = response.data
-      console.log(this.datosPerfil);
-    })
-  }
+obtenerDatosPerfil() {
+  this.apis.userProfile().subscribe(response => {
+    this.datosPerfil = response.data
+  })
+}
 
-  obtenerProductos() {
-    this.apis.allProducts().subscribe(response => {
-      this.productos = response.data
-      console.log(this.productos);
-    })
+obtenerProductos() {
+  this.apis.allProducts().subscribe(response => {
+    this.productos = response.data
+    this.ontenerOfertasDelDia();
+  })
+}
+
+ontenerOfertasDelDia(){
+  for(var x = 0;x<this.productos.length;x++){
+    if(this.productos[x].discount > 0 ){
+      this.productos[x]["precionSinDescuento"] = parseFloat(this.productos[x].discount) + parseFloat(this.productos[x].product_price)
+      this.ofertas.push(this.productos[x]);
+    }
   }
+}
+
+  
 
 }
