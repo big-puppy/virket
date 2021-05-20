ff9d81d55c1c05dbeea4ca9e1cd1f37aecdf8bc4
Gabriel hernandez <jgabrielohm@gmail.com>
2021-05-20 12:12:33 -0500

    agregar producto desde el detalle

--- src/app/detalle-producto/detalle-producto.page.ts
+++ src/app/detalle-producto/detalle-producto.page.ts
@@ -1,24 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { ActivatedRoute } from '@angular/router'
+import { Storage } from '@ionic/storage';
+import { IonicToastService } from '../services/ionic-toast.service';
 
 @Component({
   selector: 'app-detalle-producto',
   templateUrl: './detalle-producto.page.html',
   styleUrls: ['./detalle-producto.page.scss'],
 })
-export class DetalleProductoPage implements OnInit {
+export class DetalleProductoPage {
+
+  MSG_AGREGAR_PRODUCTO = "Producto agregado correctamente"
 
   producto = []
+  carrito = { products: [], subtotal: 0, shipping: "", total: 0 }
 
-  constructor(private navCtrl: NavController, private route: ActivatedRoute) {
+  constructor(private navCtrl: NavController, private route: ActivatedRoute, private storage: Storage, public toastController: IonicToastService) { }
+
+  ionViewDidEnter() {
     this.obtenerProductoInicio();
+    this.obtenerCarritoLocal();
   }
 
-  ngOnInit() {
-
-  }
-
+  /**
+   * Obtiene el producto seleccionado en la pantalla inicio
+   */
   obtenerProductoInicio() {
     this.route.queryParams.subscribe(params => {
       this.producto = JSON.parse(params["producto"])
@@ -26,8 +33,44 @@ export class DetalleProductoPage implements OnInit {
     })
   }
 
+  /**
+   * Regresa a inicio
+   */
   regresar() {
     this.navCtrl.back();
+  }
+
+  /*************************CARRITO************************/
+
+  /**
+   * busca si hay carrito en el local storage
+   */
+  obtenerCarritoLocal() {
+    this.storage.get('carrito').then((val => {
+      this.carrito = val;
+    }));
+  }
+
+  /**
+  * Agregar prodcuto entrante al carrito local
+  * se crea la propiedad color y se asigna la posicion del color seleccionado de colors
+  * se elimina la propiedad colors
+  * @param producto 
+  */
+  agregarAlCarrito(producto, i) {
+    producto["color"] = producto.colors[i]
+    delete producto.colors;
+    this.carrito.products.push(producto);
+    this.guardarCarritoLocal();
+    this.toastController.showToast(this.MSG_AGREGAR_PRODUCTO)
+    this.regresar();
+  }
+
+  /**
+ * Guarda los prodcutos en el carrito del local storage
+ */
+  guardarCarritoLocal() {
+    this.storage.set("carrito", this.carrito)
   }
 
 }
