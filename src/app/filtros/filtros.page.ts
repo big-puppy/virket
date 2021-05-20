183e60e1fa2d79ccda4624c30ccc763802883bd5
Gabriel hernandez <jgabrielohm@gmail.com>
2021-05-20 17:48:26 -0500

    funcionalidad para mostrar productos y ver su detalle

--- src/app/filtros/filtros.page.ts
+++ src/app/filtros/filtros.page.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage';
+import { NavController } from '@ionic/angular';
+import { NavigationExtras } from '@angular/router';
 
 @Component({
   selector: 'app-filtros',
   templateUrl: './filtros.page.html',
   styleUrls: ['./filtros.page.scss'],
 })
-export class FiltrosPage implements OnInit {
+export class FiltrosPage {
 
-  constructor() { }
+  productos = [];
+  prodcutosCopia = [];
 
-  ngOnInit() {
+  constructor(private storage: Storage, private navCtrl: NavController) { }
+
+  ionViewDidEnter(){
+    this.obtenerProductosLocales();
   }
+
+  /**
+   * busca si hay productos en el local storage
+   */
+  obtenerProductosLocales() {
+    this.storage.get('productos').then((val => {
+      this.productos = val;
+      this.prodcutosCopia = this.productos;
+    }));
+  }
+
+   /**
+   * Con base al producto entrante navegamos a la pagina detalle-producto y podemos
+   * ver el detalle de ese producto
+   * @param producto 
+   */
+    verDetalleProducto(producto) {
+      let navigationExtras: NavigationExtras = {
+        queryParams: {
+          producto: JSON.stringify(producto)
+        }
+      }
+      this.navCtrl.navigateRoot(["detalle-producto"], navigationExtras);
+    }
 
 }
